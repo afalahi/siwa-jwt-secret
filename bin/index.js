89bf0d464d973e7fe0a039e72bfd8973a1e0fd66
Ali Falahi <ali@afalahi.com>
2021-11-04 12:15:34 -0600

    added readme

--- bin/index.js
+++ bin/index.js
@@ -3,7 +3,7 @@
 const chalk = require('chalk');
 const yargs = require('yargs');
 const jwt = require('jsonwebtoken');
-const fs= require('fs');
+const fs = require('fs');
 
 const options = yargs
   .usage('Usage: -k <private key>')
@@ -42,29 +42,35 @@ const options = yargs
     demandOption: true,
   }).argv;
 
-  const key =fs.readFile(options.key)
-jwt.sign(
-  { iat: Date.now() },
-  key,
-  {
-    algorithm: 'ES256',
-    keyid: options.keyid,
-    issuer: options.iss,
-    audience: options.aud,
-    expiresIn: '500y',
-    subject: options.sub,
-    header: { typ: undefined },
-  },
-  (err, token) => {
-    if (err) {
-      console.error(err);
-    }
-    const splitToken = token.split('.');
-    const purple = chalk.hex('#d63aff');
-    red = chalk.hex('#fb015b');
-    cyan = chalk.hex('#00b9f1');
-    console.log(
-      `${red(splitToken[0])}.${purple(splitToken[1])}.${cyan(splitToken[2])}`
-    );
+fs.readFile(options.key, (err, data) => {
+  if (err) {
+    console.error(err.message);
+    return
   }
-);
+  jwt.sign(
+    { iat: Date.now() },
+    data,
+    {
+      algorithm: 'ES256',
+      keyid: options.keyid,
+      issuer: options.iss,
+      audience: options.aud,
+      expiresIn: '500y',
+      subject: options.sub,
+      header: { typ: undefined },
+    },
+    (err, token) => {
+      if (err) {
+        console.error(err);
+        return
+      }
+      const splitToken = token.split('.');
+      const purple = chalk.hex('#d63aff');
+      red = chalk.hex('#fb015b');
+      cyan = chalk.hex('#00b9f1');
+      console.log(
+        `${red(splitToken[0])}.${purple(splitToken[1])}.${cyan(splitToken[2])}`
+      );
+    }
+  );
+});
---
+++ readME.md
@@ -0,0 +1,21 @@
+# siwa-jwt-secret
+
+Sign in with Apple JWT generator. This is a tiny utility that will help you generate the JWT secret needed by Apple for Sign In With Apple (SIWA)
+
+---
+## Requirements
+
+You need nodeJS and NPM installed to use this utility
+
+## Install
+
+    $ npm install -g siwa-jwt-secret
+
+## Usage
+    $ swia-jwt -a <audience> -k <key file name/path> -d <key id> -i <issuer> -s <subject>
+
+## Aliases
+    $ swia-jwt --aud <audience> --key <key file name/path> --keyid <key id> --iss <issuer> --sub <subject>
+
+## Dependencies
+This tool relies on jsonwebtoken, yargs, and chalk
